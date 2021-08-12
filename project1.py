dee19614cac3fdf32a4d5d7e48644164188320c9
hac <70646122+hac01@users.noreply.github.com>
2021-08-12 19:34:20 +0530

    day1 project

---
+++ project1.py
@@ -0,0 +1,4 @@
+print("Welcome To Our World Class Band Name Generator")
+city=input("Tell Me The City In Which You Grew Up In? \n")
+pet=input("What's your pet name? \n ")
+print("Here's Your Awesome Band Name " + city+" "+ pet)
